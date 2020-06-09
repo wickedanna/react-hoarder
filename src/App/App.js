0abfece885f735ea18f29f2e1ee23dce5dbf19bc
Kayla Melton <kannmelton2@gmail.com>
2020-06-08 19:43:10 -0500

    completed auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import fbConnection from '../helpers/data/connection';
+
+import Auth from '../components/Auth/Auth';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
     return (
       <div className="App">
-        <h2>Inside App Component</h2>
-        <button className="btn btn-info">hoarder</button>
+        <h2>React Hoarder</h2>
+        <MyNavbar />
+        <Auth />
       </div>
     );
   }
