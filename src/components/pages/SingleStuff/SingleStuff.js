b4e30954af789ddaed52813b3a67ffe3bac1c51c
Kayla Melton <kannmelton2@gmail.com>
2020-06-11 22:03:42 -0500

    can delete item from single view

--- src/components/pages/SingleStuff/SingleStuff.js
+++ src/components/pages/SingleStuff/SingleStuff.js
@@ -15,6 +15,13 @@ class SingleStuff extends React.Component {
       .catch((err) => console.error('could not get single item: ', err));
   }
 
+  removeItem = () => {
+    const { itemId } = this.props.match.params;
+    stuffData.deleteItem(itemId)
+      .then(() => this.props.history.push('/home'))
+      .catch((err) => console.error('could not delete item: ', err));
+  }
+
   render() {
     const { item } = this.state;
 
@@ -23,6 +30,9 @@ class SingleStuff extends React.Component {
         <h2 className="my-3 col-12">{item.itemName}</h2>
         <img className="col-6 m-auto single-item-img" src={item.itemImage} alt={item.itemDescription} />
         <h5 className="col-4 mt-5 m-auto">{item.itemDescription}</h5>
+        <div className="my-3 col-12">
+          <button className="btn btn-danger" onClick={this.removeItem}><i className="far fa-trash-alt"></i></button>
+        </div>
       </div>
     );
   }
